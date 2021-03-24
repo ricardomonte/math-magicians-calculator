622c1d3bcba652f3a5c5fa8c97f74e00c05f1852
ricardo <ramn21@gmail.com>
2021-03-24 20:52:49 +0100

    Add Calculator.js to test functionality

---
+++ src/Calculator.js
@@ -0,0 +1,9 @@
+function Calculator(props) {
+  return(
+    <div>
+      <h1>hello world {props.name}</h1>
+    </div>
+  );
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Calculator from './Calculator'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator name={'ricardo'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
