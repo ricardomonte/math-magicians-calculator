7e86059a4a2b11f14d660f66c55d92f7d947d82b
ricardo <ramn21@gmail.com>
2021-03-26 00:42:38 +0100

    Import Calculate

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
 function App() {
+  const test = Calculate({ total: '5', next: 1, operator: '+' }, '=');
+  console.log(test);
   return (
     <>
       <Display />
