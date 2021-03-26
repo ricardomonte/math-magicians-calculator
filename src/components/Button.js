6ae85ddf2fbde4602c7998d047aefea5dd2dd3aa
ricardo <ramn21@gmail.com>
2021-03-26 22:23:47 +0100

    Add onClick to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 
-function Button({ name }) {
+function Button({ name, onClick }) {
   return (
-    <button type="button">{ name }</button>
+    <button type="button" onClick={() => onClick(name)}>{ name }</button>
   );
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+
 };
 
 export default Button;
