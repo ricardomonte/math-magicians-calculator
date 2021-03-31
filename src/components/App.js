f99f1caf4932977a64bd66b8f378b3c6841a10cb
ricardo <ramn21@gmail.com>
2021-03-31 21:23:21 +0200

    Change from class  to function component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [state, setState] = useState({ total: null, next: null, operator: null });
 
-    this.state = {
-      total: null,
-      next: null,
-      operator: null,
-    };
-  }
+  const handleClick = (simbolOperation) => {
+    setState(Calculate(state, simbolOperation));
+  };
 
-  handleClick = (simbolOperation) => {
-    const result = Calculate(this.state, simbolOperation);
-    this.setState(result);
-  }
-
-  render() {
-    const { total, next, operator } = this.state;
+  const result = () => {
+    const { total, next, operator } = state;
     let display = '0';
     if (next) {
       display = total + operator + next;
@@ -29,14 +20,15 @@ class App extends Component {
     } else {
       display = total || '0';
     }
+    return display;
+  };
 
-    return (
-      <div className="w-25 bg-white">
-        <Display calculation={display} />
-        <ButtonPanel onClick={this.handleClick} />
-      </div>
-    );
-  }
+  return (
+    <div className="w-25 bg-white">
+      <Display calculation={result()} />
+      <ButtonPanel onClick={handleClick} />
+    </div>
+  );
 }
 
 export default App;
