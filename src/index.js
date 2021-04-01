9761c07b9a1c22082dda9bd6baa6e3f751e3b6f2
ricardo <ramn21@gmail.com>
2021-04-01 22:42:13 +0200

    Change import App for import Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Routes from './Routes';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root'),
 );
